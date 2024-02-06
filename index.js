c8244493e63ee2a2cae73a34dec7fda9c64efdeb
sivaprasad2848 <sivaprasad@cybersquare.org>
2024-02-06 15:40:14 +0530

    Added New Files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const http=require('http');
 const fs=require('fs');
 const url=require('url');
@@ -27,3 +28,29 @@ const server=http.createServer((req,res)=>{
 server.listen(3500,()=>{
     console.log('Server is running @3500');
 });
+=======
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'text/html'})
+
+    var s = url.parse(req.url,true)
+    if(s.pathname == '/enterchat'){
+        fs.readFile('./enterchat.html',(err,data) =>{
+            if(err){
+                return res.end("<p align='center></p>")
+            }else{
+                return res.end(data)
+            }
+        })
+    }
+    if(s.pathname)
+    if(s.pathname == '/lostchat'){
+        res.write('Display chat');
+    }
+});
+server.listen(3500,()=>{
+    console.log('server is running');
+})
+>>>>>>> cbc2971425afb269d80771d60934a2bc0c584251
