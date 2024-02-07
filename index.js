63051ac10271b7911ab6665d922728aa5017a052
sivaprasad2848 <sivaprasad@cybersquare.org>
2024-02-07 09:49:57 +0530

    Data Updated

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const http=require('http');
+const url=require('url');
+const routes = require("./routes");
 const server=http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end("<h1>Working</h1>");
+    const parsedUrl=url.parse(req.url,true);
+    const query= parsedUrl.query;
+    const path=parsedUrl.pathname;
+    const method=req.method.toLocaleUpperCase();
+    let handler=routes[path]&&routes[path][method];
+    console.log(handler);
+    //res.writeHead(200,{'Content-Type':'text/html'});
+    //res.end("<h1>Hello</h1>");
+    handler(req,res);
 });
-server.listen(3500,()=>{
+server.listen(3600,()=>{
     console.log('Server is working @3500');
 });
