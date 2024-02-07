e6520c3860bfc192771dd85f110e656c038d332d
sivaprasad2848 <sivaprasad@cybersquare.org>
2024-02-07 19:20:43 +0530

    Server Side Events Updated

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const url=require('url');
 const fs=require("fs");
 const server=http.createServer((req,res)=>{
     const path=url.parse(req.url,true);
-    res.writeHead(200,{'Content-Type':'text/html'});
+    
     console.log(path.pathname);
     
     if(path.pathname=="/chatentry")
     {
+        res.writeHead(200,{'Content-Type':'text/html'});
         fs.readFile('./enterchat.html',(error,data)=>{
             if(error){
                 return  res.write("<p>Error</p>");
@@ -18,20 +19,33 @@ const server=http.createServer((req,res)=>{
         });
     }
     if(path.pathname=="/postchat"){
+        res.writeHead(200,{'Content-Type':'text/html'});
         const date = new Date();
         let datastr="<p>"+path.query.txtName+":"+path.query.txtMsg+":"+date.getDate()+"-"+date.getMonth()+"-"+date.getFullYear()+"/"+date.getHours()+":"+date.getMinutes()+"</p><br>";
         fs.appendFile('chatTxt.txt',datastr,()=>{});
         return res.end("<h1>Chat Entered</h1>");
     }
     if(path.pathname=="/getchat"){
-        fs.readFile('./chatTxt.txt',(error,data)=>{
-            if(error){
-                return  res.write("<p>Error</p>");
-            }
-            return res.end(data);
-
-
+        res.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            Connection: 'keep-alive',
+            'Cache-Control': 'no-cache',
         });
+        setInterval(()=>{
+            fs.readFile('./chatTxt.txt',(error,data)=>{
+                if(error){
+                    return  res.write("<p>Error</p>");
+                }
+                s='event:message \n';
+                s+='data:'+data;
+                return res.end(s);
+
+    
+    
+            });
+        },5000);
+        req.on('close', () => res.end('Chat Closed'))
+        
     }
 
 
